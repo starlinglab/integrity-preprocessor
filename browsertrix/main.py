f93b427dceb059aa68776244bdf080ee2590e80c
makeworld <makeworld@protonmail.com>
2022-03-24 16:21:06 -0400

    Cleanup, simplify, rename vars

--- browsertrix/main.py
+++ browsertrix/main.py
@@ -1,137 +1,113 @@
 from zipfile import ZipFile
-import tempfile
 import requests
 import datetime
 import json
 import shutil
 import os
-from os.path import basename
+from collections import defaultdict
 import time
-
 
 SOURCEPATH = "/mnt/browsertrix"
 TARGETPATH = "/mnt/browsertrix-out"
-BUCKET = "test-bucket"
 
+BUCKET = os.environ.get("BUCKET", "test-bucket")
+USERNAME = os.environ.get("BROWSERTRIX_USERNAME")
+PASSWORD = os.environ.get("BROWSERTRIX_PASSWORD")
+HOST = os.environ.get("BROWSERTRIX_HOST", "127.0.0.1:9871")
+TMP_DIR = os.environ.get("TMP_DIR", "/tmp/browstertrix-preprocessor")
 
-# print(response.json())
+last_check = {}
 
-lastCheck = {}
+os.makedirs(TMP_DIR, exist_ok=True)
 
+while True:
+    metrics = defaultdict(lambda: 0)
 
-def incMetric(key):
-    if not key in metrics:
-        metrics[key] = 0
-    metrics[key] += 1
-
-
-while 1:
-    metrics = {}
-
-    auth = {"username": "yurko@hypha.coop", "password": "83UF3iAf87mj9fS"}
-    response = requests.post("http://127.0.0.1:9871/api/auth/jwt/login", data=auth)
+    auth = {"username": USERNAME, "password": PASSWORD}
+    response = requests.post(f"http://{HOST}/api/auth/jwt/login", data=auth)
     access_token = response.json()["access_token"]
     headers = {"Authorization": "Bearer " + access_token}
 
-    response = requests.get("http://127.0.0.1:9871/api/archives", headers=headers)
+    response = requests.get(f"http://{HOST}/api/archives", headers=headers)
 
     for archive in response.json()["archives"]:
         aid = archive["id"]
 
-        if not aid in lastCheck:
-            lastCheck[aid] = 0
+        if not aid in last_check:
+            last_check[aid] = 0
 
         response = requests.get(
-            "http://127.0.0.1:9871/api/archives/" + aid + "/crawls", headers=headers
+            f"http://{HOST}/api/archives/" + aid + "/crawls", headers=headers
         )
         for crawl in response.json()["crawls"]:
 
-            incMetric("crawl_state_" + crawl["state"])
+            metrics["crawl_state_" + crawl["state"]] += 1
 
             if crawl["state"] == "complete":
-                startDate = datetime.datetime.fromisoformat(crawl["started"])
-                finishDate = datetime.datetime.fromisoformat(crawl["finished"])
-                startDateString = startDate.strftime("%Y-%m-%d%H%M")
-                finishDate = datetime.datetime.fromisoformat(crawl["finished"])
-
-                length = finishDate - startDate
+                start_date = datetime.datetime.fromisoformat(crawl["started"])
+                finish_date = datetime.datetime.fromisoformat(crawl["finished"])
+                length = finish_date - start_date
 
                 if length.total_seconds() / 60 < 1:
-                    incMetric("crawl_slow_count")
+                    metrics["crawl_slow_count"] += 1
 
-                if finishDate.timestamp() > lastCheck[aid]:
-                    lastCheck[aid] = finishDate.timestamp()
+                if finish_date.timestamp() > last_check[aid]:
+                    last_check[aid] = finish_date.timestamp()
                     print(crawl["id"])
 
                     response = requests.get(
-                        "http://127.0.0.1:9871/api/archives/"
-                        + aid
-                        + "/crawls/"
-                        + crawl["id"]
-                        + ".json",
+                        f"http://{HOST}/api/archives/{aid}/crawls/{crawl['id']}.json",
                         headers=headers,
                     )
-                    crawlReponse = response.json()
+                    crawl_reponse = response.json()
 
-                    # path = SOURCEPATH + "/" + BUCKET + "/" + aid + '/data/' + startDateString + "*" + crawl["id"] + '.wacz'
-                    waczPath = (
-                        SOURCEPATH
-                        + "/"
-                        + BUCKET
-                        + "/"
-                        + crawlReponse["resources"][0]["name"]
+                    wacz_path = os.path.join(
+                        SOURCEPATH, BUCKET, crawl_reponse["resources"][0]["name"]
                     )
-                    meta = {}
-                    with ZipFile(waczPath, "r") as waczFile:
-                        with tempfile.TemporaryDirectory() as td:
-                            waczFile.extract("datapackage-digest.json", td)
-                            with open(td + "/datapackage-digest.json") as json_file:
-                                data = json.load(json_file)
-                                print(data)
-                                meta["authsign_software"] = data["signedData"][
-                                    "software"
-                                ]
-                                meta["authsign_domain"] = data["signedData"]["domain"]
-                            # print(data)
-                            waczFile.extract("datapackage.json", td)
-                            with open(td + "/datapackage.json") as json_file:
-                                data = json.load(json_file)
-                                meta["wacz_version"] = data["wacz_version"]
-                                meta["created"] = data["created"]
-                            # print(data)
-                            waczFile.extract("pages/pages.jsonl", td)
-                            meta["pages"] = {}
-                            with open(td + "/pages/pages.jsonl") as jsonl_file:
-                                for jsonLine in jsonl_file.readlines():
-                                    data = json.loads(jsonLine)
-                                    if "url" in data:
-                                        meta["pages"][data["id"]] = data["url"]
-                            f = open(td + "/metadata.json", "w")
-                            f.write(json.dumps(meta))
-                            f.close
 
-                            f = open(td + "/metadata_injestor.json", "w")
-                            f.write(json.dumps({}))
-                            f.close
+                    content_meta = {}
+                    recorder_meta = {}
 
-                            zipObj = ZipFile(
-                                td + "/" + crawlReponse["id"] + ".zip", "w"
-                            )
-                            zipObj.write(waczPath, basename(waczPath))
-                            zipObj.write(td + "/metadata.json", "metadata.json")
-                            zipObj.write(
-                                td + "/metadata_injestor.json",
-                                "/metadata_injestor.json",
-                            )
-                            zipObj.close
+                    with ZipFile(wacz_path, "r") as wacz:
 
-                            shutil.move(
-                                td + "/" + crawlReponse["id"] + ".zip",
-                                TARGETPATH + "/" + crawlReponse["id"] + ".zip.part",
-                            )
-                            os.rename(
-                                TARGETPATH + "/" + crawlReponse["id"] + ".zip.part",
-                                TARGETPATH + "/" + crawlReponse["id"] + ".zip",
-                            )
+                        data = json.loads(wacz.read("datapackage-digest.json"))
+                        print(data)
+                        content_meta["authsign_software"] = data["signedData"][
+                            "software"
+                        ]
+                        content_meta["authsign_domain"] = data["signedData"]["domain"]
+
+                        data = json.loads(wacz.read("datapackage.json"))
+                        content_meta["wacz_version"] = data["wacz_version"]
+                        content_meta["created"] = data["created"]
+
+                        content_meta["pages"] = {}
+                        with wacz.open("pages/pages.jsonl") as jsonl_file:
+                            for line in jsonl_file.readlines():
+                                data = json.loads(line)
+                                if "url" in data:
+                                    content_meta["pages"][data["id"]] = data["url"]
+
+                        zip_path = os.path.join(TMP_DIR, crawl_reponse["id"] + ".zip")
+                        zipf = ZipFile(zip_path, "w")
+                        zipf.write(wacz_path, os.path.basename(wacz_path))
+                        zipf.writestr("content_metadata.json", json.dumps(content_meta))
+                        zipf.writestr(
+                            "recorder_metadata.json", json.dumps(recorder_meta)
+                        )
+                        zipf.close()
+
+                        # Moving is not atomic in this case because the destination
+                        # is on a mounted filesystem. So use a .part file.
+                        shutil.move(
+                            zip_path,
+                            os.path.join(TARGETPATH, crawl_reponse["id"] + ".zip.part"),
+                        )
+                        os.rename(
+                            os.path.join(TARGETPATH, crawl_reponse["id"] + ".zip.part"),
+                            os.path.join(TARGETPATH, crawl_reponse["id"] + ".zip"),
+                        )
 
     print(metrics)
+
+    time.sleep(60)
