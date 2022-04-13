311227cf6633509d234dcf1385d2da57984d49f5
Yurko <Yurko@noreply.starlinglab.org>
2022-04-13 15:25:00 -0400

    Updated metadata to follow new schema

--- integrity_recorder_id/integrity_recorder_id.py
+++ integrity_recorder_id/integrity_recorder_id.py
@@ -3,6 +3,7 @@ import sys
 import json
 from subprocess import getoutput
 import dotenv
+import datetime
 
 def dockerComposeHash(repoPath):
     os.chdir(repoPath)
@@ -15,27 +16,51 @@ def dockerComposeHash(repoPath):
     docker_lines.pop(0)
     docker_lines.pop(0)
 
+    docker_data = []
+
     for line in docker_lines:
         if len(line) > 1:
+
             items = line.split()
-            docker_sha = getoutput(
-                "docker inspect --format='{{.Image}}' " + items[0]
-            )
-            docker_services[items[0]] = docker_sha
-    return docker_services
+            docker_sha = getoutput("docker inspect --format='{{.Image}}' " + items[0])
+            docker_name = items[0]
+
+            docker_item = {
+                "type": "docker",
+                "values": {
+                    "name": docker_name,
+                    "image": docker_sha,
+                },
+            }
+            docker_data.append(docker_item)
+
+    return docker_data
+
 
 def gitHash(repoPath):
     os.chdir(repoPath)
 
-    gitHash = {}
     git_raw = getoutput("git rev-parse HEAD")
     git_branch = getoutput("git remote -v")
     git_branchsplit = git_branch.split()
-    gitHash["repo"] = os.path.basename(git_branchsplit[1])
+    git_repository = os.path.basename(git_branchsplit[1])
 
     git_branch = getoutput("git branch -v")
     git_branchsplit = git_branch.split()
-    gitHash["branch"] = git_branchsplit[1]
+    git_branch = git_branchsplit[1]
+
+    git_commit = ""
+    git_clean = False
+
+    gitHash = {
+        "type": "git",
+        "values": {
+            "repository": git_repository,
+            "branch": git_branch,
+            "commit": git_commit,
+            "clean": git_clean,
+        },
+    }
 
     return gitHash
 
@@ -45,21 +70,25 @@ def build_recorder_id_json():
     f = open(INTEGRITY_PREPROCESSOR_CONFIG_PATH)
     recorder_configs = json.load(f)
 
-    integrity = {}
+    integrity = {"recorderMetadata": []}
 
     for key in recorder_configs:
         recorder_config = recorder_configs[key]
-        integrity[key] = {}
-        integrity[key]["type"] = recorder_config["type"]
-        if "dockercompose" in recorder_config and recorder_config["dockercompose"]:
-            integrity[key]["docker_services"] = dockerComposeHash(
-                recorder_config["path"]
-            )
-        if "git" in recorder_config and recorder_config["git"]:
-            integrity[key]["github"] = gitHash(recorder_config["path"])
 
-        with open(INTEGRITY_PREPROCESSOR_TARGET_PATH, "w") as outfile:
-            json.dump(integrity, outfile)
+        recorder = {"service": key, "info": []}
+        recorder_type = recorder_config["type"]
+        if "dockercompose" in recorder_config and recorder_config["dockercompose"]:
+            recorder["info"] = dockerComposeHash(recorder_config["path"])
+        if "git" in recorder_config and recorder_config["git"]:
+            recorder["info"].append(gitHash(recorder_config["path"]))
+
+        integrity["recorderMetadata"].append(recorder)
+
+    # +Z becuase python doesnt support Z like Javascript Does
+    integrity['timestamp']=datetime.datetime.utcnow().isoformat() + "Z"     
+    with open(INTEGRITY_PREPROCESSOR_TARGET_PATH, "w") as outfile:
+        json.dump(integrity, outfile, indent=4)
+
 
 dotenv.load_dotenv()
 INTEGRITY_PREPROCESSOR_CONFIG_PATH = os.environ.get(
