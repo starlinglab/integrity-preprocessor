35b59a450ed5b2f5227d38258a79ec678594858a
Yurko <39072689+YurkoWasHere@users.noreply.github.com>
2022-05-09 22:20:30 -0400

    remove contentMetadata key

--- folder/main.py
+++ folder/main.py
@@ -76,7 +76,7 @@ def generate_metadata_content(
     meta_content["extras"] = extras
     meta_content["private"] = private
 
-    return {"contentMetadata": meta_content}
+    return meta_content
 
 
 metdata_file_timestamp = -1
