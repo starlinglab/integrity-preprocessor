44222e040acbf97da214b20b6c3effc20bbe5890
Yurko <Yurko@noreply.starlinglab.org>
2022-05-30 16:02:41 -0400

    Refactor common Switch to RAISE instead of loops implement logging

--- browsertrix/main.py
+++ browsertrix/main.py
@@ -20,7 +20,7 @@ import sys
 sys.path.append(
     os.path.dirname(os.path.realpath(__file__)) + "/../lib"
 )
-import integrity_recorder_id
+import common
 
 dotenv.load_dotenv()
 
@@ -49,8 +49,6 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 logging.info("Started browsertrix preprocessor")
-
-
 
 # Load config
 config_data = {}
@@ -84,32 +82,6 @@ if not os.path.exists(TARGET_PATH_TMP["default"]):
 metdata_file_timestamp = 0
 
 
-def prepare_metadata_recorder():
-    global metdata_file_timestamp, recorder_meta_all
-
-    current_metadata_file_timestamp = os.path.getmtime(
-        integrity_recorder_id.INTEGRITY_PREPROCESSOR_TARGET_PATH
-    )
-
-    if current_metadata_file_timestamp > metdata_file_timestamp:
-        if os.path.exists(integrity_recorder_id.INTEGRITY_PREPROCESSOR_TARGET_PATH):
-            with open(
-                integrity_recorder_id.INTEGRITY_PREPROCESSOR_TARGET_PATH, "r"
-            ) as f:
-                recorder_meta_all = json.load(f)
-                print("Recorder Metadata Change Detected")
-                metdata_file_timestamp = current_metadata_file_timestamp
-    # TODO remove unused recorders
-    return recorder_meta_all
-
-
-def sha256sum(filename):
-    with open(filename, "rb") as f:
-        bytes = f.read()  # read entire file as bytes
-        readable_hash = hashlib.sha256(bytes).hexdigest()
-        return readable_hash
-
-
 default_author = {
     "@type": "Organization",
     "identifier": "https://starlinglab.org",
@@ -123,48 +95,6 @@ default_content = {
     "author": default_author,
 }
 
-#########################################
-# Turn into a module, shared with dropbox#
-# #######################################
-def processWacz(wacz_path):
-    # WACZ metadata extraction
-    with ZipFile(wacz_path, "r") as wacz:
-        d = json.loads(wacz.read("datapackage-digest.json"))
-        extras = {}
-
-        if "signedData" in d:
-            # auth sign data
-            if "authsignDomain" in d["signedData"]:
-                extras["authsignSoftware"] = d["signedData"]["software"]
-                extras["authsignDomain"] = d["signedData"]["domain"]
-            elif "publicKey" in d["signedData"]:
-                extras["localsignSoftware"] = d["signedData"]["software"]
-                extras["localsignPublicKey"] = d["signedData"]["publicKey"]
-                extras["localsignSignaturey"] = d["signedData"]["signature"]
-            else:
-                logging.info("WACZ missing signature ")
-
-        d = json.loads(wacz.read("datapackage.json"))
-        extras["waczVersion"] = d["wacz_version"]
-        extras["software"] = d["software"]
-        extras["dateCrawled"] = d["created"]
-
-        if "title" in d:
-            extras["waczTitle"] = d["title"]
-
-        extras["pages"] = {}
-        if "pages/pages.jsonl" in wacz.namelist():
-            with wacz.open("pages/pages.jsonl") as jsonl_file:
-                for line in jsonl_file.readlines():
-                    d = json.loads(line)
-                    if "url" in d:
-                        extras["pages"][d["id"]] = d["url"]
-        else:
-            logging.info("Missing pages/pages.jsonl in archive %s", aid)
-
-        return extras
-
-
 def generate_metadata_content(
     meta_crawl_config, meta_crawl_data, meta_additional, meta_extra, meta_date_created
 ):
@@ -176,6 +106,9 @@ def generate_metadata_content(
     private = {}
     if "private" in meta_additional:
         private.update(meta_additional["private"])
+    sourceId = None
+    if sourceId in meta_additional:
+        sourceId = meta_additional["sourceId"]
 
     private["crawlConfigs"] = meta_crawl_config
     private["crawlData"] = meta_crawl_data
@@ -204,9 +137,11 @@ def generate_metadata_content(
     meta_content["dateCreated"] = meta_date_created
     meta_content["extras"] = extras
     meta_content["private"] = private
+    if (sourceId):
+        meta_content["sourceId"] = sourceId
     meta_content["timestamp"] = datetime.utcnow().isoformat() + "Z"
 
-    return {"contentMetadata": meta_content}
+    return meta_content
 
 
 def send_to_prometheus(metrics):
@@ -309,12 +244,7 @@ def update_crawl_config(cid, aid, data):
             json=data,
         )
         if r.status_code != 200:
-            log_req_err(r)
-            i += 1
-            time.sleep(FAIL_DELAY)
-            continue
-        log_req_success(r, i)
-        break
+            raise Exception(f"PATCH of {aid}/crawlconfigs Faild")
     return r.json()
 
 
@@ -326,11 +256,7 @@ def get_crawl_config(cid, aid):
             headers=headers(),
         )
         if r.status_code != 200:
-            log_req_err(r)
-            i += 1
-            time.sleep(FAIL_DELAY)
-            continue
-        log_req_success(r, i)
+            raise Exception(f"GET of {aid}/crawlconfigs Faild")
         break
     return r.json()
 
@@ -357,9 +283,6 @@ if os.path.exists(DATA_JSON_PATH):
 # Write initial file
 send_to_prometheus(metrics)
 
-# Update IDs
-integrity_recorder_id.build_recorder_id_json()
-
 while True:
 
     i = 1
@@ -385,6 +308,9 @@ while True:
 
         logging.info("Working on archive %s", aid)
 
+
+       
+
         if not aid in data:
             data[aid] = {"last_check": 0, "crawls": []}
 
@@ -392,19 +318,13 @@ while True:
         new_crawls = data[aid]["crawls"]
 
         i = 1
-        while True:
-            r = requests.get(
-                f"{BROWSERTRIX_URL}/api/archives/{aid}/crawls", headers=headers()
-            )
-            if r.status_code != 200:
-                log_req_err(r, i)
-                i += 1
-                time.sleep(FAIL_DELAY)
-                continue
-            log_req_success(r, i)
-            break
-
+        r = requests.get(
+            f"{BROWSERTRIX_URL}/api/archives/{aid}/crawls", headers=headers()
+        )        
+        if r.status_code != 200:
+            raise Exception(f"GET of /api/archives/{aid}/crawls failed")
         crawls = r.json()["crawls"]
+
 
         # Count the number of currently running crawls
         for crawl in crawls:
@@ -449,18 +369,13 @@ while True:
                 metrics["crawl_short_count"] += 1
 
             i = 1
-            while True:
-                r = requests.get(
-                    f"{BROWSERTRIX_URL}/api/archives/{aid}/crawls/{crawl['id']}.json",
-                    headers=headers(),
-                )
-                if r.status_code != 200:
-                    log_req_err(r)
-                    i += 1
-                    time.sleep(FAIL_DELAY)
-                    continue
-                log_req_success(r, i)
-                break
+            r = requests.get(
+                f"{BROWSERTRIX_URL}/api/archives/{aid}/crawls/{crawl['id']}.json",
+                headers=headers(),
+            )
+            if r.status_code != 200:
+                raise Exception(f"GET of /api/archives/{aid}/crawls/{crawl['id']}.json Failed")
+
 
             crawl_json = r.json()
 
@@ -492,7 +407,7 @@ while True:
                 time.sleep(FAIL_DELAY)
 
             # Meta data collection and generation
-            recorder_meta = prepare_metadata_recorder()
+            recorder_meta = common.get_recorder_meta("browsertrix")            
 
             meta_additional = ""
             meta_crawl = ""
@@ -512,7 +427,7 @@ while True:
                 f = open(meta_additional_filename)
                 meta_additional = json.load(f)
 
-            meta_extra = processWacz(wacz_path)
+            meta_extra = common.parse_wacz_data_extra(wacz_path)
             meta_date_created = crawl_json["started"]
 
             content_meta = generate_metadata_content(
@@ -523,96 +438,11 @@ while True:
                 meta_date_created,
             )
 
-            sha256wacz = sha256sum(wacz_path)
-            zip_path = os.path.join(TMP_DIR, sha256wacz + ".zip")
-
             i = 1
-            while True:
-                try:
-                    with ZipFile(zip_path, "w") as zipf:
-                        zipf.write(wacz_path, sha256wacz + ".wacz")
-                        zipf.writestr(
-                            sha256wacz + "-meta-content.json", json.dumps(content_meta)
-                        )
-                        zipf.writestr(
-                            sha256wacz + "-meta-recorder.json",
-                            json.dumps(recorder_meta),
-                        )
-                except OSError:
-                    logging.exception(
-                        "Failed to write temp ZIP to %s (tries: %d)", TMP_DIR, i
-                    )
-                    metrics["tmp_zip_write_failures"] += 1
-                    i += 1
-                    time.sleep(FAIL_DELAY)
-                else:
-                    logging.info("Wrote temp ZIP to %s (tries: %d)", TMP_DIR, i)
-                    break
 
-            # Moving is not atomic in this case because the destination
-            # is on a mounted filesystem. So use a .part file to copy the file
-            # contents, then rename on the mounted filesystem.
-
-            i = 1
-            while True:
-                try:
-                    shutil.move(
-                        zip_path,
-                        os.path.join(
-                            TARGET_PATH_TMP[current_collection],
-                            sha256wacz + ".zip.part",
-                        ),
-                    )
-                except (OSError, shutil.Error, PermissionError):
-                    logging.exception(
-                        "Failed to move temp ZIP to %s (tries: %d)",
-                        TARGET_PATH_TMP[current_collection],
-                        i,
-                    )
-                    metrics["tmp_zip_move_failures"] += 1
-                    i += 1
-                    time.sleep(FAIL_DELAY)
-                else:
-                    logging.info(
-                        "Moved temp ZIP to %s (tries: %d)",
-                        TARGET_PATH_TMP[current_collection],
-                        i,
-                    )
-                    break
-
-            i = 1
-            sha256zip = sha256sum(
-                os.path.join(
-                    TARGET_PATH_TMP[current_collection], sha256wacz + ".zip.part"
-                )
-            )
-            while True:
-                try:
-                    os.rename(
-                        os.path.join(
-                            TARGET_PATH_TMP[current_collection],
-                            sha256wacz + ".zip.part",
-                        ),
-                        os.path.join(
-                            TARGET_PATH[current_collection], sha256zip + ".zip"
-                        ),
-                    )
-                except OSError:
-                    logging.exception(
-                        "Failed to rename .zip.part to .zip for crawl %s (tries: %d)",
-                        crawl["id"],
-                        i,
-                    )
-                    metrics["tmp_zip_rename_failures"] += 1
-                    i += 1
-                    time.sleep(FAIL_DELAY)
-                else:
-                    logging.info(
-                        "Renamed .zip.part to .zip for crawl %s (tries: %d)",
-                        crawl["id"],
-                        i,
-                    )
-                    break
+            out_file = common.add_to_pipeline(wacz_path, content_meta, recorder_meta, TARGET_PATH_TMP[current_collection], TARGET_PATH[current_collection])
+            sha256zip = os.path.splitext(os.path.basename(out_file))[0]
+          
 
             # Write the ID to a file for refrence
             if os.path.exists(meta_additional_filename):
@@ -639,11 +469,7 @@ while True:
     while True:
         r = requests.get(f"{BROWSERTRIX_URL}/api/archives", headers=headers())
         if r.status_code != 200:
-            log_req_err(r, i)
-            i += 1
-            time.sleep(FAIL_DELAY)
-            continue
-        log_req_success(r, i)
+            raise Exception(f"GET of api/archives Failed")
         break
 
     queuelist = []
@@ -654,11 +480,7 @@ while True:
                 f"{BROWSERTRIX_URL}/api/archives/{aid}/crawlconfigs", headers=headers()
             )
             if r.status_code != 200:
-                log_req_err(r, i)
-                i += 1
-                time.sleep(FAIL_DELAY)
-                continue
-            log_req_success(r, i)
+                raise Exception(f"GET of {aid}/crawlconfigs Failed")
             break
 
         # Check if crawl is to be queued and add it to array
@@ -682,11 +504,11 @@ while True:
             headers=headers(),
         )
         crawl_running_count = crawl_running_count + 1
-        print(f"Started crawl of {aid}/{cid}")
+        logging.info(f"Started crawl of {aid}/{cid}")
         crawlid = r.json()["started"]
         new_name = "_R_" + name
         data = {"name": new_name}
         r = update_crawl_config(cid, aid, data)
-        print(r)
+        logging.info(r)
 
     time.sleep(LOOP_INTERVAL)
