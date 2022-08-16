27909de8561cf93da393f595356447270f5389cb
makeworld <makeworld@protonmail.com>
2022-08-16 17:17:06 -0400

    EOD: updated Starling Capture code for 1.9.2, wrote tests for sw sig only

--- lib/validate/starling_capture.py
+++ lib/validate/starling_capture.py
@@ -1,3 +1,12 @@
+# This file validates sigs from the Starling Capture android app
+# https://github.com/numbersprotocol/starling-capture
+#
+# There are several different kinds of signatures that need to be validated
+# The wiki describes them: https://github.com/numbersprotocol/starling-capture/wiki/Development#verification
+#
+# This code is loosely based on the upstream verification code:
+# https://github.com/numbersprotocol/starling-capture/blob/1.9.2/util/verification/verification/verification.py
+
 from hashlib import sha256
 import json
 
@@ -29,12 +38,13 @@ class StarlingCapture(Validate):
         self.asset_path = asset_path
         self.meta_raw = meta_raw
         self.meta = json.loads(self.meta_raw)
-        self.meta_hash = sha256(self.meta_raw.encode()).hexdigest()
+        self.meta_hash_bytes = sha256(self.meta_raw.encode()).digest()
+        self.meta_hash = self.meta_hash_bytes.hex()
         self.sigs = sigs
         self.json = []
         self.provider = "starling-capture"
 
-    def name() -> str:
+    def name(self) -> str:
         return "starling-capture"
 
     def validate(self) -> bool:
@@ -52,14 +62,11 @@ class StarlingCapture(Validate):
             Any errors raised due to signature bytes being malformed
         """
 
-        meta = json.loads(self.meta_raw)
-
-        return self._validate_create_hashes(
-            self.asset_path, meta, self.sigs
-        ) and self._validate_all_sigs(self.meta_raw, self.sigs)
+        return self._validate_create_hashes() and self._validate_all_sigs()
 
     def _validate_create_hashes(self) -> bool:
-        """Validate asset hashes in the data sent to the create action.
+        """
+        Validate asset hashes in the data sent to the create action.
 
         Returns:
             True if the hashes matched the asset, False if not
@@ -117,82 +124,163 @@ class StarlingCapture(Validate):
                     }
                 )
             elif sig["provider"] == "Zion":
-                if not self._validate_zion(self.meta_raw, sig):
-                    return False
-                self.json.append(
-                    {
-                        "provider": self.provider,
-                        "algorithm": "starling-capture-Zion-session"
-                        if sig["publicKey"].startswith("Session")
-                        else "starling-capture-Zion",
-                        "signature": sig["signature"],
-                        "publicKey": sig["publicKey"],
-                        "authenticatedMessage": self.meta_hash,
-                        "authenticatedMessageDescription": self.auth_msg_desc,
-                    }
-                )
+                # Four options:
+                # Old app (<1.9.2): Zion classic, Zion session classic
+                # New app (1.9.2): Zion, Zion session
+
+                if len(sig["signature"]) == 130:
+                    # The signature is Zion signature (not session-based)
+                    # Try Zion then Zion classic and succeed if either works
+                    # No way to tell them apart except by trying
+
+                    if self._validate_zion(self.meta_hash, sig):
+                        self.json.append(
+                            {
+                                "provider": self.provider,
+                                "algorithm": "starling-capture-Zion",
+                                "signature": sig["signature"],
+                                "publicKey": sig["publicKey"],
+                                "authenticatedMessage": self.meta_hash,
+                                "authenticatedMessageDescription": self.auth_msg_desc,
+                            }
+                        )
+                    elif self._validate_zion_classic(self.meta_hash_bytes, sig):
+                        self.json.append(
+                            {
+                                "provider": self.provider,
+                                "algorithm": "starling-capture-Zion-classic",
+                                "signature": sig["signature"],
+                                "publicKey": sig["publicKey"],
+                                "authenticatedMessage": self.meta_hash,
+                                "authenticatedMessageDescription": self.auth_msg_desc,
+                            }
+                        )
+                    else:
+                        return False
+
+                elif (
+                    "Session" in sig["publicKey"]
+                    and "SessionSignature" not in sig["publicKey"]
+                ):
+                    # The signature is Zion session-based classic signature
+                    if not self._validate_zion_session_classic(self.meta_raw, sig):
+                        return False
+                    self.json.append(
+                        {
+                            "provider": self.provider,
+                            "algorithm": "starling-capture-Zion-session-classic",
+                            "signature": sig["signature"],
+                            "publicKey": sig["publicKey"],
+                            "authenticatedMessage": self.meta_hash,
+                            "authenticatedMessageDescription": self.auth_msg_desc,
+                        }
+                    )
+
+                elif (
+                    "Session" in sig["publicKey"]
+                    and "SessionSignature" in sig["publicKey"]
+                ):
+                    # The signature is Zion session-based signature
+                    if not self._validate_zion_session(self.meta_raw, sig):
+                        return False
+                    self.json.append(
+                        {
+                            "provider": self.provider,
+                            "algorithm": "starling-capture-Zion-session",
+                            "signature": sig["signature"],
+                            "publicKey": sig["publicKey"],
+                            "authenticatedMessage": self.meta_hash,
+                            "authenticatedMessageDescription": self.auth_msg_desc,
+                        }
+                    )
+
+                else:
+                    raise Exception(
+                        f"Unrecognized Zion signature with length {len(sig['signature'])}"
+                    )
+
             else:
+                # Not AndroidOpenSSL or Zion provider
                 raise NotImplementedError(f"Provider {sig['provider']} not implemented")
+
         return True
 
     def validated_sigs_json(self, short=False) -> list:
         return self._shorten(self.json) if short else self.json
 
     @staticmethod
-    def _validate_androidopenssl(meta_raw: str, signature: dict) -> bool:
+    def _verify_ecdsa_sig(msg: str, key_hex: str, sig_hex: str) -> bool:
         # Adapted from signature verification example
         # https://www.pycryptodome.org/en/latest/src/signature/dsa.html
 
-        key = ECC.import_key(bytes.fromhex(signature["publicKey"]))
-        h = SHA256.new(meta_raw.encode())
+        key = ECC.import_key(bytes.fromhex(key_hex))
+        h = SHA256.new(msg.encode())
         verifier = DSS.new(key, "fips-186-3", encoding="der")
         try:
-            verifier.verify(h, bytes.fromhex(signature["signature"]))
+            verifier.verify(h, bytes.fromhex(sig_hex))
             return True
         except ValueError:
             return False
 
     @staticmethod
-    def _validate_zion(meta_raw: str, signature: dict) -> bool:
-        # Content of publicKey field is generated by app
-        # Source code is here: https://github.com/starlinglab/starling-capture/blob/fbf55abb205444d5f067d048ff5c5c89682f94b4/app/src/main/java/io/numbersprotocol/starlingcapture/collector/zion/ZionSessionSignatureProvider.kt#L24-L40
-        # Either the publicKey field starts with "Session" and the data is signed
-        # by an ecdsa session key, or it starts with "Receive" and then the data is
-        # signed with an ethereum signature thing.
+    def _validate_androidopenssl(meta_raw: str, signature: dict) -> bool:
+        return StarlingCapture._verify_ecdsa_sig(
+            meta_raw, signature["publicKey"], signature["signature"]
+        )
 
-        if signature["publicKey"].startswith("Session"):
-            # Same as _validate_androidopenssl
-            key = ECC.import_key(
-                bytes.fromhex(
-                    # Extract key from after "Session" label
-                    # 182 is the length of a hex-encoded, DER-encoded ec key
-                    signature["publicKey"][
-                        len("Session:\n") : len("Session:\n") + 182 + 1
-                    ]
-                )
-            )
-            h = SHA256.new(meta_raw.encode())
-            verifier = DSS.new(key, "fips-186-3", encoding="der")
-            try:
-                verifier.verify(h, bytes.fromhex(signature["signature"]))
-                return True
-            except ValueError:
-                return False
+    @staticmethod
+    def _validate_zion_classic(meta_hash_bytes: bytes, signature: dict) -> bool:
+        message = encode_defunct(meta_hash_bytes)
+        # pylint: disable=no-value-for-parameter
+        addr = Account.recover_message(message, signature="0x" + signature["signature"])
+        # Signer's Ethereum address
+        pk = PublicKey.from_compressed_bytes(
+            bytes.fromhex(signature["publicKey"][-66:])
+        )
+        return addr == pk.to_checksum_address()
 
-        elif signature["publicKey"].startswith("Receive"):
-            # Ethereum signature using ethereum key
-            # Result is ethereum address
+    @staticmethod
+    def _validate_zion_session_classic(meta_raw: str, signature: dict) -> bool:
+        # Extract key from after "Session" label
+        # 182 is the length of a hex-encoded, DER-encoded EC key
+        key_hex = signature["publicKey"][
+            len("Session:\n") : len("Session:\n") + 182 + 1
+        ]
+        return StarlingCapture._verify_ecdsa_sig(
+            meta_raw, key_hex, signature["signature"]
+        )
 
-            message = encode_defunct(text=meta_raw)
-            # Ethereum address the signature belongs to
-            addr = Account.recover_message(message, signature=signature["signature"])
-            # Signer's Ethereum address
-            pk = PublicKey.from_compressed_bytes(
-                bytes.fromhex(signature["publicKey"][-66:])
-            )
-            return addr == pk.to_checksum_address()
+    @staticmethod
+    def _validate_zion(meta_hash_hex: str, signature: dict) -> bool:
+        message = encode_defunct(text=meta_hash_hex)
+        # pylint: disable=no-value-for-parameter
+        addr = Account.recover_message(message, signature="0x" + signature["signature"])
+        # Signer's Ethereum address
+        pk = PublicKey.from_compressed_bytes(
+            bytes.fromhex(signature["publicKey"][-66:])
+        )
+        return addr == pk.to_checksum_address()
 
-        else:
-            raise Exception(
-                "Zion publicKey doesn't start with Session or Receive, unable to parse"
-            )
+    @staticmethod
+    def _validate_zion_session(meta_raw: str, signature: dict) -> bool:
+        lines = signature["publicKey"].split("\n")
+        session_public_key = lines[1]
+        zion_session_sig = lines[4]
+
+        # First verify session key software signature is valid
+        session_sig_valid = StarlingCapture._verify_ecdsa_sig(
+            meta_raw, session_public_key, signature["signature"]
+        )
+        if not session_sig_valid:
+            return False
+
+        # Next verify Zion sig of session key
+        # Similar to _validate_zion
+        message = sha256(session_public_key.encode()).hexdigest()
+        # pylint: disable=no-value-for-parameter
+        addr = Account.recover_message(message, signature="0x" + zion_session_sig)
+        # Signer's Ethereum address
+        pk = PublicKey.from_compressed_bytes(
+            bytes.fromhex(signature["publicKey"][-66:])
+        )
+        return addr == pk.to_checksum_address()
--- tests/test_starling_capture.py
+++ tests/test_starling_capture.py
@@ -1,124 +1,29 @@
+import os
 import json
+
 from .context import validate
 
 
-def test_validate_create_hashes():
-    with open("tests/assets/starling-capture/good/session/signature.json", "r") as f:
+def sc_from_dir(dir_path: str) -> validate.StarlingCapture:
+    dir_path = os.path.join("tests/assets/starling-capture", dir_path)
+
+    asset_path = os.path.join(dir_path, os.path.basename(dir_path) + ".jpg")
+
+    with open(os.path.join(dir_path, "information.json"), "r") as f:
+        meta_raw = f.read()
+    with open(os.path.join(dir_path, "signature.json"), "r") as f:
         sigs = json.load(f)
-    with open("tests/assets/starling-capture/good/session/meta.json", "r") as f:
-        meta_raw = f.read()
-    sc = validate.StarlingCapture(
-        "tests/assets/starling-capture/good/session/asset.jpg", meta_raw, sigs
+
+    return validate.StarlingCapture(asset_path, meta_raw, sigs)
+
+
+def test_validate_androidopenssl():
+    sc = sc_from_dir(
+        "official-examples/1.8.0/034305235db1ac3fcd6580765f893c53e3aea07e7f2c3096600cb533ac92e94f"
     )
-    assert sc._validate_create_hashes() == True
-
-
-def test_validate_androidopenssl_session():
-    with open("tests/assets/starling-capture/good/session/signature.json", "r") as f:
-        signatures = json.load(f)
-    with open("tests/assets/starling-capture/good/session/meta.json", "r") as f:
-        meta_raw = f.read()
-    assert (
-        validate.StarlingCapture._validate_androidopenssl(meta_raw, signatures[0])
-        == True
-    )
-
-
-def test_validate_bad_androidopenssl_session():
-    with open("tests/assets/starling-capture/bad/session/signature.json", "r") as f:
-        signatures = json.load(f)
-    with open("tests/assets/starling-capture/bad/session/meta.json", "r") as f:
-        meta_raw = f.read()
-    assert (
-        validate.StarlingCapture._validate_androidopenssl(meta_raw, signatures[0])
-        == False
-    )
-
-
-def test_validate_androidopenssl_no_session():
-    with open("tests/assets/starling-capture/good/no-session/signature.json", "r") as f:
-        signatures = json.load(f)
-    with open("tests/assets/starling-capture/good/no-session/meta.json", "r") as f:
-        meta_raw = f.read()
-    assert (
-        validate.StarlingCapture._validate_androidopenssl(meta_raw, signatures[0])
-        == True
-    )
-
-
-def test_validate_bad_androidopenssl_no_session():
-    with open("tests/assets/starling-capture/bad/no-session/signature.json", "r") as f:
-        signatures = json.load(f)
-    with open("tests/assets/starling-capture/bad/no-session/meta.json", "r") as f:
-        meta_raw = f.read()
-    assert (
-        validate.StarlingCapture._validate_androidopenssl(meta_raw, signatures[0])
-        == False
-    )
-
-
-def test_validate_zion_session():
-    with open("tests/assets/starling-capture/good/session/signature.json", "r") as f:
-        signatures = json.load(f)
-    with open("tests/assets/starling-capture/good/session/meta.json", "r") as f:
-        meta_raw = f.read()
-    assert validate.StarlingCapture._validate_zion(meta_raw, signatures[1]) == True
-
-
-def test_validate_bad_zion_session():
-    with open("tests/assets/starling-capture/bad/session/signature.json", "r") as f:
-        signatures = json.load(f)
-    with open("tests/assets/starling-capture/bad/session/meta.json", "r") as f:
-        meta_raw = f.read()
-    assert validate.StarlingCapture._validate_zion(meta_raw, signatures[1]) == False
-
-
-def test_validate_zion_no_session():
-    with open("tests/assets/starling-capture/good/no-session/signature.json", "r") as f:
-        signatures = json.load(f)
-    with open("tests/assets/starling-capture/good/no-session/meta.json", "r") as f:
-        meta_raw = f.read()
-    assert validate.StarlingCapture._validate_zion(meta_raw, signatures[1]) == True
-
-
-def test_validate_bad_zion_no_session():
-    with open("tests/assets/starling-capture/bad/no-session/signature.json", "r") as f:
-        signatures = json.load(f)
-    with open("tests/assets/starling-capture/bad/no-session/meta.json", "r") as f:
-        meta_raw = f.read()
-    assert validate.StarlingCapture._validate_zion(meta_raw, signatures[1]) == False
+    assert sc.validate()
 
 
 def test_validated_sigs_json():
-    with open("tests/assets/starling-capture/good/session/signature.json", "r") as f:
-        signatures = json.load(f)
-    with open("tests/assets/starling-capture/good/session/meta.json", "r") as f:
-        meta_raw = f.read()
-
-    sc = validate.StarlingCapture("", meta_raw, signatures)
-    assert sc._validate_all_sigs() == True
-    assert sc.validated_sigs_json() == [
-        {
-            "algorithm": "starling-capture-AndroidOpenSSL",
-            "authenticatedMessage": "b20b2237c3768491244feb04dd59be0a1bcf730beb6436f775ab0419490c7299",
-            "authenticatedMessageDescription": "SHA256 hash of meta.json",
-            "provider": "starling-capture",
-            "publicKey": "3059301306072a8648ce3d020106082a8648ce3d03010703420004dea6b33c2747d8a53fcf66404f0a46197effe42abbade6b213dd8ee86b9e0857b9f03479bf4abce6a2373e3e46642b82f0fd8f68098c5227e84615168c7638af",
-            "signature": "304502210095136726e7bf83b0ad9042e859ee9ba2b7935bfc49af87100cd3d424d85caa25022079c7bbe9cef99a46e3b17eeef72c04231f91e9bc74bee54c476bcb3d86ea8d63",
-        },
-        {
-            "algorithm": "starling-capture-Zion-session",
-            "authenticatedMessage": "b20b2237c3768491244feb04dd59be0a1bcf730beb6436f775ab0419490c7299",
-            "authenticatedMessageDescription": "SHA256 hash of meta.json",
-            "provider": "starling-capture",
-            "publicKey": "Session:\n"
-            "3059301306072a8648ce3d020106082a8648ce3d03010703420004247e5b35ac7e8b54b191098fb0e1a931543a634a6cf70389e3b7946b87e4e38f190e59d38a4b6de518da84058bfaa9c35321dae0e7b19329a7b0aaa56363ec27\n"
-            "\n"
-            "Receive:\n"
-            "02fd5c07443aa3e87dc271981dee52a8c15d74a086394f63603bb6b0836ec5f811\n"
-            "\n"
-            "Send:\n"
-            "02fd5c07443aa3e87dc271981dee52a8c15d74a086394f63603bb6b0836ec5f811",
-            "signature": "3045022100ba94892728e9b7ae7cdc47293fbb681a06e4a7de70f3acbe1880ff704cb6def5022071e7b9ec34cdd60930047d2e3e7e7cb11ced151066532b7bcee8036514a35fff",
-        },
-    ]
+    # TODO
+    pass
