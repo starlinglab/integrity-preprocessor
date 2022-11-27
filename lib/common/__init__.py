878187ce0915c16a364f8fd6cac991da31520bd2
Yurko <39072689+YurkoWasHere@users.noreply.github.com>
2022-11-27 17:54:48 -0500

    Update __init__.py

--- lib/common/__init__.py
+++ lib/common/__init__.py
@@ -11,6 +11,7 @@ from warcio.archiveiterator import ArchiveIterator
 
 import sys
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/../")
+import validate
 import integrity_recorder_id
 
 integrity_recorder_id.build_recorder_id_json()
