0b440cc086b78a188a2dbca73a940418132769e3
YurkoWasHere <yurkowashere@users.noreply.github.com>
2023-07-04 22:43:05 -0400

    Attempts 2

--- tests/test_common_init.py
+++ tests/test_common_init.py
@@ -1,11 +1,17 @@
 import os
+
+DEBUG = os.environ.get("HTTP_DEBUG") == "1"
 import sys
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../lib")))
 import common
 
-DEBUG = os.environ.get("HTTP_DEBUG") == "1"
 
 def test_sha256sum():
-    assert common.sha256sum("tests/assets/sha256_test.txt") == "936a185caaa266bb9cbe981e9e05cb78cd732b0b3280eb944412bb6f8f8f07af"
+    assert (
+        common.sha256sum("tests/assets/sha256_test.txt")
+        == "936a185caaa266bb9cbe981e9e05cb78cd732b0b3280eb944412bb6f8f8f07af"
+    )
 
-#def add_to_pipeline(source_file, content_meta, recorder_meta, stage_path, output_path):
+
+# def add_to_pipeline(source_file, content_meta, recorder_meta, stage_path, output_path):
--- tests/test_common_metadata.py
+++ tests/test_common_metadata.py
@@ -1,8 +1,10 @@
 import json
 import os
-import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../lib")))
+import sys
+
+
 import common
 
 DEBUG = os.environ.get("HTTP_DEBUG") == "1"
